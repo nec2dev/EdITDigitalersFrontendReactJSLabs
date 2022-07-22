253c511f7ea645559f1ea6b890111ebf767c6185
Nahuel Correa <nec2multivackergithub@outlook.com>
2022-07-22 19:45:53 -0300

    mod004-Lab01 OK

--- mod004-Lab001/js/main.js
+++ mod004-Lab001/js/main.js
@@ -3,29 +3,29 @@
 sóla función. Cada botón pasará a un color
 distinto. La referencia es la de arriba a la
 derecha: */
-function colorearCaja(color){
+/* function alertColor(color){
     alert('El color es: ' + color);
-}
+} */
 /* 2. Al hacer clic en cada botón, haremos la alerta
 que vemos a la derecha. */
 let boton = document.getElementsByTagName("button");
 for (let i = 0; i < boton.length; i++) {
-  boton[i].onclick = function(){
-    colorearCaja(this.innerHTML);
-  };
-  boton[i].onmouseover = function(){
-    crearCaja(this.innerHTML);
+  boton[i].onclick = function () {
+    alertColor(this.innerHTML);
   };
 }
 /* 3. Generamos una caja, y dentro trabajaremos
 con innerHTML para poder sumar el texto
 como en el ejemplo de referencia. */
-function crearCaja(texto){
-    let caja = document.createElement("div");
-    caja.innerHTML = texto;
-    caja.className = "caja";
-    document.body.appendChild(caja);
-}
+
 /* 4. Con style modificaremos el color de fondo de
 la caja. */
+
+function alertColor(color) {
+  alert("El color es " + color);
+  document.getElementById("caja").innerHTML = "El color es " + color;
+  document.getElementById("caja").style.background = color;
+}
+
+
 
