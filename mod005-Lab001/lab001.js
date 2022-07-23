5745e2207e429dc8d5f98e239908c1240a75513f
Nahuel Correa <nec2multivackergithub@outlook.com>
2022-07-22 21:20:23 -0300

    mod005-Lab01 Correct Version

--- mod005-Lab001/lab001.js
+++ mod005-Lab001/lab001.js
@@ -1,6 +1,15 @@
-var numeroActual = 0;
+/* var numeroActual = 0;
 do {
     var numeroActual = Number(prompt("Ingrese un numero actual"));
     var suma = suma + numeroActual;
     } while ((continuar=confirm("Desea seguir ingresando numeros?")));
+alert("Total Ingresado: " + suma); */
+
+let numeroActual = 0;
+let suma = 0;
+do {
+    numeroActual = Number(prompt("Ingrese un numero actual"));
+    suma += numeroActual;
+    } while ((continuar=confirm("Desea seguir ingresando numeros?")));
 alert("Total Ingresado: " + suma);
+
